5d05120962612711804cba7c9493c9c43155f75e
ltulyavko <wot.k@mail.ru>
2024-03-19 15:12:33 +0300

    wip

--- pkg/datasets/datasets_vtproto.pb.go
+++ pkg/datasets/datasets_vtproto.pb.go
@@ -456,6 +456,13 @@ func (m *Dataset) MarshalToSizedBufferVT(dAtA []byte) (int, error) {
 		i -= len(m.unknownFields)
 		copy(dAtA[i:], m.unknownFields)
 	}
+	if len(m.UploadError) > 0 {
+		i -= len(m.UploadError)
+		copy(dAtA[i:], m.UploadError)
+		i = protohelpers.EncodeVarint(dAtA, i, uint64(len(m.UploadError)))
+		i--
+		dAtA[i] = 0x42
+	}
 	if m.UpdatedAt != nil {
 		size, err := (*timestamppb.Timestamp)(m.UpdatedAt).MarshalToSizedBufferVT(dAtA[:i])
 		if err != nil {
@@ -985,6 +992,10 @@ func (m *Dataset) SizeVT() (n int) {
 	}
 	if m.UpdatedAt != nil {
 		l = (*timestamppb.Timestamp)(m.UpdatedAt).SizeVT()
+		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
+	}
+	l = len(m.UploadError)
+	if l > 0 {
 		n += 1 + l + protohelpers.SizeOfVarint(uint64(l))
 	}
 	n += len(m.unknownFields)
@@ -2201,6 +2212,38 @@ func (m *Dataset) UnmarshalVT(dAtA []byte) error {
 			if err := (*timestamppb.Timestamp)(m.UpdatedAt).UnmarshalVT(dAtA[iNdEx:postIndex]); err != nil {
 				return err
 			}
+			iNdEx = postIndex
+		case 8:
+			if wireType != 2 {
+				return fmt.Errorf("proto: wrong wireType = %d for field UploadError", wireType)
+			}
+			var stringLen uint64
+			for shift := uint(0); ; shift += 7 {
+				if shift >= 64 {
+					return protohelpers.ErrIntOverflow
+				}
+				if iNdEx >= l {
+					return io.ErrUnexpectedEOF
+				}
+				b := dAtA[iNdEx]
+				iNdEx++
+				stringLen |= uint64(b&0x7F) << shift
+				if b < 0x80 {
+					break
+				}
+			}
+			intStringLen := int(stringLen)
+			if intStringLen < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			postIndex := iNdEx + intStringLen
+			if postIndex < 0 {
+				return protohelpers.ErrInvalidLength
+			}
+			if postIndex > l {
+				return io.ErrUnexpectedEOF
+			}
+			m.UploadError = string(dAtA[iNdEx:postIndex])
 			iNdEx = postIndex
 		default:
 			iNdEx = preIndex
